8b91873ebc18800694b9e55bf1cde8bf2a2009ce
Guilherme Miranda <guimiranda@live.com>
2015-02-18 23:14:35 -0200

    Initial Commit

---
+++ Caliwoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Caliwoo
+//
+//  Created by Guilherme Miranda on 18/02/15.
+//  Copyright (c) 2015 KinGui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
